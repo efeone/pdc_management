3bd241cfc5772005f682b3b9a70f71cfecc40c22
SherinKR <sherinkrply@gmail.com>
2026-08-03 12:32:43 +0530

    feat: added company in post dated cheque

--- pdc_management/pdc_management/doctype/post_dated_cheque/post_dated_cheque.js
+++ pdc_management/pdc_management/doctype/post_dated_cheque/post_dated_cheque.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2025, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Post Dated Cheque", {
-// 	refresh(frm) {
+frappe.ui.form.on("Post Dated Cheque", {
+	setup: function(frm) {
+		set_filters(frm);
+	},
+});
 
-// 	},
-// });
+function set_filters(frm) {
+	frm.set_query("party_type", function () {
+		return {
+			filters: {
+				name: ["in", ["Customer", "Supplier"]],
+			},
+		};
+	});
+}
